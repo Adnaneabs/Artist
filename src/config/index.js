6425ed7761b8cb83afdb335ed86f7af23117b53b
AREalex <alexissy@outlook.fr>
2022-04-11 13:12:32 +0200

    Error resolved
    Previously error resolved

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const config = {
-        contractAddress: "KT1QWd7u7aC31noV5v2GrDYBCCXWZFyqWpqg",
-        tokenAdress: "KT1F67bvdj4v3B4i6mgjmmMVN2Gr3ZFBgu1J"
+        contractAddress: "KT1Bz5PtGAtZsfboWWYby98xxKPjtKVHgDAw",
+        tokenAddress: "KT1F67bvdj4v3B4i6mgjmmMVN2Gr3ZFBgu1J"
 }
 
 export default config;
