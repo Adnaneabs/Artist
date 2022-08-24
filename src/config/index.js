b32f27fac3787e9987a988e19c84e9f0db3b60f9
Ouissem-redjemi <ouissem.redjemi@gmail.com>
2022-08-24 12:06:53 +0200

    Modification du contrat

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,9 @@
 const config = {
-  contractAddress: "KT1Bz5PtGAtZsfboWWYby98xxKPjtKVHgDAw",
-  tokenAddress: "KT1F67bvdj4v3B4i6mgjmmMVN2Gr3ZFBgu1J",
+  contractAddress: "KT19rNH2JG8UgYrVzsLuCmTbZgszHqVfnUd4	",
+  tokenAddress: "KT1EPGU4F1ChnxuVwDftkRGXQB5dJCzhqHuf",
 };
 // contractaddress Tacos : KT1Bz5PtGAtZsfboWWYby98xxKPjtKVHgDAw
 //tokenAddress ;: KT1F67bvdj4v3B4i6mgjmmMVN2Gr3ZFBgu1J
 // contractaddress nouveau (gardez le propre) : KT1TK524BKbTFCqGrDo2ZJNxGsJmCpRpUhUR
-//KT1KGXyBJgp3tzUQG74TeYnj5MRSsWDXbd3d   KT1PkoZ11uCyAsmaofwssFECR83kAHwfeXom
 //V2 pour mint et collect : KT1TgcztTqgtciarCJ7jkEgP96fucezpaecE
 export default config;
